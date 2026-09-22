ae7eb0ce11f2545afa9b7eefbf7ce95c52b641d8
kanna219 <murakamikanna05@gmail.com>
2026-09-22 10:14:13 -0600

    in class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,8 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour tout le monde.");
-        Console.WriteLine("Hey, Jude.");
-        Console.WriteLine("Hey, Jay.");
+        int x = 10;
+        int y = 30;
+        int z = 40;
+
+        if (x == 10 || y == 30 && z == 30)
+        {
+            Console.WriteLine("X is 10"); 
+            Console.WriteLine("Y is fun.");
+        }
+        else if (x == 20)
+        {
+            Console.WriteLine("X is 10");
+        }
+        else
+        {
+            Console.WriteLine("Default output");
+        }
+
+        string numberString = "123";
+        int myNumber = int.Parse(numberString);
     }
 }
